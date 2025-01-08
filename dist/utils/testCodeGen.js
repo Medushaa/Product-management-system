76565919a6b653c60faa5fc2bfe378b4ae2e43f9
Masuda <Masudarahman27@gmail.com>
2025-01-08 19:31:11 +0600

    added .gitignore

--- dist/utils/testCodeGen.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const productCode_1 = require("./productCode");
-//Testing on a example string
-const productName = "Alpha Sorter";
-const productCode = (0, productCode_1.generateProductCode)(productName);
-console.log(`Product Name: ${productName}`);
-console.log(`Generated Product Code: ${productCode}`); //p48asd4-0alport8
-//run it with: npx ts-node testCodeGen.ts
